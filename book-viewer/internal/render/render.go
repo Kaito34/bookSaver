45e5f1e98f247752d86e4c898e1b852c89e5c38b
Kaito34 <zihhkumi@icloud.com>
2023-02-10 19:36:42 +0900

    [add] web page

---
+++ book-viewer/internal/render/render.go
@@ -0,0 +1,104 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/Kaito34/bookSaver/internal/config"
+	"github.com/Kaito34/bookSaver/internal/models"
+	"github.com/justinas/nosurf"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+var pathToTemplate = "./templates"
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Error = app.Session.PopString(r.Context(), "error")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
+	td.CSRFToken = nosurf.Token(r)
+	return td
+}
+
+// RenderTemplate renders templates using http/template
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// create a template cache
+		// get the template cache from
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	// get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("could not get template from template cache")
+	}
+
+	// ???
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td, r)
+
+	err := t.Execute(buf, td)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// render the template
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	// myCache := make(map[string]*template.Template)
+	myCache := map[string]*template.Template{}
+
+	// get all of the files named *.page.tmpl from ./templates
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplate))
+	if err != nil {
+		return myCache, err
+	}
+
+	// range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplate))
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplate))
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
+
